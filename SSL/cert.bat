9718748c5131d96326435c40fce42d3f35640b2f
Dener Rocha <denernun@gmail.com>
2021-09-10 15:40:24 -0300

    Delete cert.bat

--- SSL/cert.bat
+++
@@ -1,6 +0,0 @@
-openssl genrsa -des3 -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem -config cert.cnf
-openssl req -new -sha256 -nodes -out localhost.csr -newkey rsa:2048 -keyout localhost.key -config cert.cnf
-openssl x509 -req -in localhost.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out localhost.crt -days 500 -sha256 -extfile cert.ext
-del localhost.csr
-del rootCA.srl
